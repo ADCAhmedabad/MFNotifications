c6748935b0e8a5f8746c0e54a3718fc646c2694b
MobileFirst <rahul@mobilefirstapplications.com>
2016-10-13 12:31:22 +0530

    Initial Commit

---
+++ RichNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichNotification
+//
+//  Created by MobileFirst Applications on 13/10/16.
+//  Copyright © 2016 MobileFirst Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

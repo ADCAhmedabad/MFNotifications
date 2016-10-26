1bfb31737bef1f73350d2080780bdf8f635929e9
Arpan Desai <arpandesai@me.com>
2016-10-26 21:02:28 +0530

    Rich Notifications

---
+++ RichNotification/General .swift
@@ -0,0 +1,9 @@
+//
+//  General .swift
+//  RichNotification
+//
+//  Created by MobileFirst Applications on 13/10/16.
+//  Copyright © 2016 MobileFirst Applications. All rights reserved.
+//
+
+import Foundation
